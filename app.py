a3943cb38f858a2763e8477d44a5eca2e5493b87
Muricael <alexandre.datascience@gmail.com>
2023-08-22 21:46:41 +0200

    Appli marche, réorga html à faire

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
+import lime
+from lime import lime_tabular
+matplotlib.use('Agg')
 
 app = Flask(__name__, static_folder='Divers', static_url_path='/Divers')
 
@@ -66,6 +69,7 @@ def generate_reports():
 #Créer la route pour la collecte de data
 @app.route('/collect_data', methods=['POST'])
 def collect_and_save_data():
+
     # Récupération des valeurs
     remboursement_str = request.form.get('REMBOURSEMENT')
     montant_emprunte_str = request.form.get('MONTANT_EMPRUNTE')
@@ -98,14 +102,18 @@ def collect_and_save_data():
     # Faire une prédiction
     try:
         proba = model.predict_proba(data_df)[0][1]
-        draw_client(proba)
+        image_path = draw_client(proba)
+        lime_image_path = draw_lime(data_df, model)
     except ValueError as e:
         prediction_result = "Erreur lors de la prédiction: {}".format(str(e))
-    
-    return render_template('home.html', prediction=proba)
+
+
+
+    return render_template('home.html', prediction=proba, image_path=image_path)
 
 
 def draw_client(accuracy_score):
+    plt.figure()
     if not np.isscalar(accuracy_score):
         raise ValueError("La valeur fournie à 'draw_gauge' doit être un scalaire.")
 
@@ -113,46 +121,99 @@ def draw_client(accuracy_score):
     normalized_score = accuracy_score
 
     # Créer un axe polar
-    fig, ax = plt.subplots(figsize=(4, 4), subplot_kw={"polar": True})
+    fig_client, ax_client = plt.subplots(figsize=(4, 4), subplot_kw={"polar": True})
 
     # Dessiner l'arc de la jauge pour la plage de 0 à 70% (en rouge)
     arc_red = np.linspace(0, 0.7 * np.pi, 100)
     r_red = np.full(100, 0.6)
-    ax.plot(arc_red, r_red, color="red", linewidth=15)
+    ax_client.plot(arc_red, r_red, color="red", linewidth=15)
 
     # Dessiner l'arc de la jauge pour la plage de 70 à 100% (en bleu)
     arc_blue = np.linspace(0.7 * np.pi, np.pi, 100)
     r_blue = np.full(100, 0.6)
-    ax.plot(arc_blue, r_blue, color="darkblue", linewidth=15)
+    ax_client.plot(arc_blue, r_blue, color="darkblue", linewidth=15)
 
     # Calculer l'angle pour l'aiguille
     theta = np.pi * (normalized_score)
-    needle, = ax.plot([theta, theta], [0, 0.6], color='darkred', linewidth=2)
+    needle, = ax_client.plot([theta, theta], [0, 0.6], color='darkred', linewidth=2)
 
     # Configurer les angles et étiquettes en pourcentage
-    ax.set_theta_zero_location("W")
-    ax.set_theta_direction(-1)
-    ax.set_xticks(np.radians([0, 45, 90, 126, 135, 180]))
-    ax.set_xticklabels(["0%", "25%", "50%","70%", "75%", "100%"])
-    ax.set_yticks([])
-    ax.set_yticklabels([])
-    ax.grid(False)
-    ax.set_ylim([0, 0.6])
-    ax.set_xlim([0, np.pi])
-    ax.set_title("EVALUATION DU CLIENT", va="bottom")
+    ax_client.set_theta_zero_location("W")
+    ax_client.set_theta_direction(-1)
+    ax_client.set_xticks(np.radians([0, 45, 90, 126, 135, 180]))
+    ax_client.set_xticklabels(["0%", "25%", "50%","70%", "75%", "100%"])
+    ax_client.set_yticks([])
+    ax_client.set_yticklabels([])
+    ax_client.grid(False)
+    ax_client.set_ylim([0, 0.6])
+    ax_client.set_xlim([0, np.pi])
+    ax_client.set_title("EVALUATION DU CLIENT", va="bottom")
 
      # Ajouter le message "CREDIT APPROUVE" ou "CREDIT REFUSE"
     credit_message = "CREDIT APPROUVE" if accuracy_score >= 0.70 else "CREDIT REFUSE"
     color = "red"
-    ax.text(0.5, 0.1, credit_message, color=color, ha="center", va="center", transform=ax.transAxes, fontsize=20, weight='bold')
+    ax_client.text(0.5, 0.1, credit_message, color=color, ha="center", va="center", transform=ax_client.transAxes, fontsize=20, weight='bold')
 
     # Ajouter le score en tant que légende
-    ax.legend([needle], [f'Score = {accuracy_score * 100:.2f}%'])
+    ax_client.legend([needle], [f'Score = {accuracy_score * 100:.2f}%'])
     
     try:
-        fig.savefig("Divers/jauge_client.png")
+        image_path = "Divers/jauge_client.png"
+        fig_client.savefig(image_path)
+        plt.close(fig_client)
+
     except Exception as e:
         print(f"Erreur lors de la sauvegarde de l'image : {e}")
+
+    return image_path
+
+def draw_lime(data_df, model):
+    
+    tables = pd.read_csv('train_data.csv')
+    X_train = tables.drop('TARGET', axis=1)
+    y_train = tables['TARGET']
+
+    explainer = lime_tabular.LimeTabularExplainer(training_data=X_train.values,
+                                                feature_names=X_train.columns.tolist(),
+                                                class_names=['0', '1'],
+                                                verbose=True, 
+                                                mode='classification')
+    sample_data = X_train.iloc[0].values
+    exp = explainer.explain_instance(sample_data, model.predict_proba, num_features=10)
+
+    # Utilisation de la méthode as_pyplot_figure pour générer le graphique
+    fig_lime = exp.as_pyplot_figure()
+
+    # Ajuster la taille de la figure
+    fig_lime.set_size_inches(10, 4)
+
+    # Récupération des dimensions actuelles et ajustement de la taille
+    current_width, current_height = fig_lime.get_size_inches()
+    fig_lime.set_size_inches(current_width * 0.8, current_height * 0.8)
+    
+
+    # Améliorations esthétiques (vous pouvez ajouter/modifier selon vos préférences)
+    ax = fig_lime.gca()
+    ax.set_facecolor('lightgray')
+    ax.grid(axis='x', linestyle='--', alpha=0.7)
+    ax.set_title("Détail du prêt")
+
+        # Changer les couleurs des barres
+    bars = ax.patches
+    for bar in bars:
+        if bar.get_facecolor()[0] == 1.0:  # Supposant que le rouge est représenté par (1, 0, 0, 1)
+            bar.set_facecolor('darkblue')
+        else:
+            bar.set_facecolor('darkred')
+
+    # Sauvegarder le graphique dans le dossier "Divers"
+    lime_image_path = "Divers/lime_explanation.png"
+    fig_lime.tight_layout()
+    fig_lime.savefig(lime_image_path)
+    plt.close(fig_lime)
+
+    return lime_image_path
+
 
 
 if __name__ == "__main__":
