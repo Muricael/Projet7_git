5b909c4c59c7ffb73d932ea0a559e68e45460c38
Muricael <alexandre.datascience@gmail.com>
2023-08-22 12:25:31 +0200

    Jauge fonctionnelle

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask, request, jsonify, render_template, render_template_stri
 from my_utilities import custom_loss
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='Divers', static_url_path='/Divers')
 
 # Charger le modèle
 loaded_data = load('xgb_model.joblib')
@@ -66,10 +69,7 @@ def collect_and_save_data():
     # Récupération des valeurs
     remboursement_str = request.form.get('REMBOURSEMENT')
     montant_emprunte_str = request.form.get('MONTANT_EMPRUNTE')
-    
-    print("Valeur de 2 REMBOURSEMENT:", remboursement_str)
-    print("Valeur de 2 MONTANT_EMPRUNTE:", montant_emprunte_str)
-    
+
     remboursement = float(remboursement_str)
     montant_emprunte = float(montant_emprunte_str)
     
@@ -99,10 +99,57 @@ def collect_and_save_data():
     try:
         proba = model.predict_proba(data_df)[0][1]
         prediction_result = "Probabilité : {:.2f}%".format(proba*100)
+        draw_client(proba)
     except ValueError as e:
         prediction_result = "Erreur lors de la prédiction: {}".format(str(e))
     
     return render_template('home.html', prediction=prediction_result)
+
+
+def draw_client(accuracy_score):
+    if not np.isscalar(accuracy_score):
+        raise ValueError("La valeur fournie à 'draw_gauge' doit être un scalaire.")
+
+    # Normaliser le score entre 0 et 1
+    normalized_score = accuracy_score
+
+    # Créer un axe polar
+    fig, ax = plt.subplots(figsize=(4, 4), subplot_kw={"polar": True})
+
+    # Dessiner l'arc de la jauge pour la plage de 0 à 70% (en rouge)
+    arc_red = np.linspace(0, 0.7 * np.pi, 100)
+    r_red = np.full(100, 0.6)
+    ax.plot(arc_red, r_red, color="red", linewidth=15)
+
+    # Dessiner l'arc de la jauge pour la plage de 70 à 100% (en bleu)
+    arc_blue = np.linspace(0.7 * np.pi, np.pi, 100)
+    r_blue = np.full(100, 0.6)
+    ax.plot(arc_blue, r_blue, color="darkblue", linewidth=15)
+
+    # Calculer l'angle pour l'aiguille
+    theta = np.pi * (normalized_score)
+    needle, = ax.plot([theta, theta], [0, 0.6], color='darkred', linewidth=2)
+
+    # Configurer les angles et étiquettes en pourcentage
+    ax.set_theta_zero_location("W")
+    ax.set_theta_direction(-1)
+    ax.set_xticks(np.radians([0, 45, 90, 126, 135, 180]))
+    ax.set_xticklabels(["0%", "25%", "50%","70%", "75%", "100%"])
+    ax.set_yticks([])
+    ax.set_yticklabels([])
+    ax.grid(False)
+    ax.set_ylim([0, 0.6])
+    ax.set_xlim([0, np.pi])
+    ax.set_title("EVALUATION DU CLIENT", va="bottom")
+
+    # Ajouter le score en tant que légende
+    ax.legend([needle], [f'Score = {accuracy_score * 100:.2f}%'])
+    
+    try:
+        fig.savefig("Divers/jauge_client.png")
+    except Exception as e:
+        print(f"Erreur lors de la sauvegarde de l'image : {e}")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
