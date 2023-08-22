088d2e7b5dc1f391c898160ba6850a88fb29d693
Muricael <alexandre.datascience@gmail.com>
2023-08-22 08:19:10 +0200

    Entrées fonctionnelles

--- app.py
+++ app.py
@@ -60,9 +60,25 @@ def generate_reports():
     generate_reports_main()
     return redirect(url_for('show_reports'))
 
-#Créer la route pour 
+#Créer la route pour la collecte de data
 @app.route('/collect_data', methods=['POST'])
 def collect_and_save_data():
+    # Récupération des valeurs
+    remboursement_str = request.form.get('REMBOURSEMENT')
+    montant_emprunte_str = request.form.get('MONTANT_EMPRUNTE')
+    
+    print("Valeur de 2 REMBOURSEMENT:", remboursement_str)
+    print("Valeur de 2 MONTANT_EMPRUNTE:", montant_emprunte_str)
+    
+    remboursement = float(remboursement_str)
+    montant_emprunte = float(montant_emprunte_str)
+    
+    # Calcul de PAYMENT_RATE
+    try:
+        payment_rate = remboursement / montant_emprunte
+    except ZeroDivisionError:
+        return "Erreur : Le montant emprunté ne peut pas être zéro."
+
     data = {
         "EXT_SOURCE_3": float(request.form.get('EXT_SOURCE_3')),
         "EXT_SOURCE_2": float(request.form.get('EXT_SOURCE_2')),
@@ -73,7 +89,7 @@ def collect_and_save_data():
         "FLAG_DOCUMENT_3": int(request.form.get('FLAG_DOCUMENT_3')),
         "AMT_REQ_CREDIT_BUREAU_HOUR": float(request.form.get('AMT_REQ_CREDIT_BUREAU_HOUR')),
         "CODE_GENDER": int(request.form.get('CODE_GENDER')),
-        "PAYMENT_RATE": float(request.form.get('PAYMENT_RATE'))
+        "PAYMENT_RATE": payment_rate  # Utilisation du taux calculé
     }
     
     # Transformer les données en DataFrame
