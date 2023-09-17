9360d0c621631581165f6f9400f8c15bdcecb42d
Muricael <alexandre.datascience@gmail.com>
2023-09-17 13:54:56 +0200

    Je comprends plus rien

--- utiles.py
+++ utiles.py
@@ -6,7 +6,6 @@ import plotly.graph_objects as go
 import lime
 from lime import lime_tabular
 import plotly.graph_objects as go
-from sklearn.metrics import confusion_matrix, make_scorer
 import evidently
 from evidently import ColumnMapping
 from evidently.report import Report
@@ -32,11 +31,6 @@ def get_credit_decision(model, new_data):
 
     return decision, probability
 
-def custom_loss(y_true, y_pred):
-    cm = confusion_matrix(y_true, y_pred)
-    fn_weight = 10.0  # poids pour les faux négatifs
-    fp_weight = 10.0  # poids pour les faux positifs
-    return cm[0, 1] * fp_weight + cm[1, 0] * fn_weight
 
 def jauge_pret_plotly(predic):
     width_of_exterior = 20  # Épaisseur des contours rouges et bleus
